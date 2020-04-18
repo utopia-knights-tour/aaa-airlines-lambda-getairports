2dec3e2bd767a94ee5fa4ea98f1f6f6714700bb3
Aland <aland.chang@smoothstack.com>
2020-04-18 17:49:38 -0400

    changed return type and junit

--- src/main/java/GetAirports.java
+++ src/main/java/GetAirports.java
@@ -5,19 +5,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
 
 import entity.Airport;
+import proxy.ApiGatewayProxyResponse;
+import proxy.ApiGatewayRequest;
 import util.ConnectUtil;
 
-public class GetAirports implements RequestHandler<Map<String, String>, Object> {
+public class GetAirports implements RequestHandler<ApiGatewayRequest, ApiGatewayProxyResponse> {
 
-	public Object handleRequest(Map<String, String> event, Context context) {
+	public ApiGatewayProxyResponse handleRequest(ApiGatewayRequest request, Context context) {
 		List<Airport> airports = new ArrayList<Airport>();
 		Connection connection = null;
+		LambdaLogger logger = context.getLogger();
 		try {
 			connection = ConnectUtil.getInstance().getConnection();
 			PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM Airport");
@@ -29,9 +33,13 @@ public class GetAirports implements RequestHandler<Map<String, String>, Object> 
 				airport.setLocation(rs.getString("airportLocation"));
 				airports.add(airport);
 			}
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
+		} catch (SQLException e) {
+			logger.log(e.getMessage());
+			return new ApiGatewayProxyResponse(400, null, null);
+		} catch (ClassNotFoundException e) {
+			logger.log(e.getMessage());
+			return new ApiGatewayProxyResponse(500, null, null);
 		}
-		return airports;
+		return new ApiGatewayProxyResponse(200, null, new Gson().toJson(airports));
 	}
 }
--- src/test/java/GetAirportsTests.java
+++ src/test/java/GetAirportsTests.java
@@ -1,18 +1,33 @@
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import entity.Airport;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+import proxy.ApiGatewayProxyResponse;
+import proxy.ApiGatewayRequest;
 
 public class GetAirportsTests {
+
+	private static Context context = null;
+	private static LambdaLogger logger = null;
+
+	@BeforeAll
+	public static void beforeAll() {
+		context = Mockito.mock(Context.class);
+		logger = Mockito.mock(LambdaLogger.class);
+		Mockito.when(context.getLogger()).thenReturn(logger);
+	}
 
 	@Test
 	public void getAirports() {
 		GetAirports getAirports = new GetAirports();
-		List<Airport> airports = (List<Airport>) getAirports.handleRequest(null, null);
-		assertFalse(airports.isEmpty());
+		ApiGatewayRequest request = Mockito.mock(ApiGatewayRequest.class);
+		ApiGatewayProxyResponse response = getAirports.handleRequest(request, context);
+		assertEquals(200, response.getStatusCode());
 	}
 
 }
