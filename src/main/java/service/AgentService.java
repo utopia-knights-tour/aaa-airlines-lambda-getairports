cc6e64503bb739cc24cd4c3fb953bd0ef260cb27
Aland <aland.chang@smoothstack.com>
2020-04-21 14:38:37 -0400

    replaced connectutil with hikaricp and closed connections

--- src/main/java/service/AgentService.java
+++ src/main/java/service/AgentService.java
@@ -5,18 +5,20 @@ import java.sql.SQLException;
 import java.util.List;
 
 import dao.AirportDao;
+import datasource.HikariCPDataSource;
 import entity.Airport;
-import util.ConnectUtil;
 
 public class AgentService {
 
 	public List<Airport> getAirports() throws ClassNotFoundException, SQLException {
 		Connection connection = null;
 		try {
-			connection = ConnectUtil.getInstance().getConnection();
+			connection = HikariCPDataSource.getConnection();
 			return new AirportDao(connection).get();
 		} catch (SQLException e) {
 			throw e;
+		} finally {
+			connection.close();
 		}
 	}
 }
