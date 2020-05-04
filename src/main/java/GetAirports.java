da9c51b1fa31f5ecc3834795c068b303148cc078
Aland <aland.chang@smoothstack.com>
2020-05-04 17:25:29 -0400

    changed max connection size

--- src/main/java/GetAirports.java
+++ src/main/java/GetAirports.java
@@ -1,6 +1,8 @@
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
@@ -18,15 +20,18 @@ public class GetAirports implements RequestHandler<ApiGatewayRequest, ApiGateway
 
 	public ApiGatewayProxyResponse handleRequest(ApiGatewayRequest request, Context context) {
 		LambdaLogger logger = context.getLogger();
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put("Access-Control-Allow-Origin", "*");
+		headers.put("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 		try {
 			List<Airport> airports = agentService.getAirports();
-			return new ApiGatewayProxyResponse(200, null, new Gson().toJson(airports));
+			return new ApiGatewayProxyResponse(200, headers, new Gson().toJson(airports));
 		} catch (SQLException e) {
 			logger.log(e.getMessage());
-			return new ApiGatewayProxyResponse(400, null, null);
+			return new ApiGatewayProxyResponse(400, headers, null);
 		} catch (ClassNotFoundException e) {
 			logger.log(e.getMessage());
-			return new ApiGatewayProxyResponse(500, null, null);
+			return new ApiGatewayProxyResponse(500, headers, null);
 		}
 	}
 }
